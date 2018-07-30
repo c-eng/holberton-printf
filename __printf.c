193a834a1aea8a7063752b6521a65c75ffe4e424
c-eng <ceng@cca.edu>
2018-07-30 16:45:37 +0000

    Removed offending semi-colon and changed malloc sizing method

--- __printf.c
+++ __printf.c
@@ -5,27 +5,47 @@
 #include <stdio.h>
 
 /**
+ * _strlen - returns length of str s
+ * @s: string to be measured
+ *
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
  * _stringf - handles string arguments for printf
  *
  */
 
 char *_stringf(char *input)
 {
-	char *output = malloc(5);
+	char *output = malloc(_strlen(input) + 1);
 	unsigned int i;
 
 	printf("_stringf input is %s\n", input);
 	printf("input is %lu big and output is %lu big\n", sizeof(input), sizeof(output));
-	for (i = 0 ; input[i] != '\0' ; i++);
+	for (i = 0 ; input[i] != '\0' ; i++)
 	{
 		printf("input %d is %c\n", i, input[i]);
 		output[i] = input[i];
 		printf("output %d is %c\n", i, output[i]);
 	}
+	output[i] = '\0';
 	printf("_stringf output is %s\n", output);
 
 	return (output);
 }
+
 
 /**
  * _printf - Main body of printf
@@ -37,7 +57,7 @@ int _printf(const char *format, ...)
 	unsigned int i = 0, j = 0, bite_count = 0;
 	va_list plist;
 	int argflag = 0, buffer_count = 0;
-	char *buffer = malloc(sizeof(char) * 1024), *store, *store2;
+	char *buffer = malloc(sizeof(char) * 1024),  *store;
 
 	va_start(plist, format);
 	while (format[i] != '\0')
@@ -58,9 +78,8 @@ int _printf(const char *format, ...)
 				case 'c':
 					break;
 				case 's':
-					store2 = va_arg(plist, char *);
-					printf("Calling _stringf on %s\n", store2);
-					store = _stringf(store2);
+					printf("Calling _stringf\n");
+					store = (char *)_stringf(va_arg(plist, void *));
 					argflag = 0;
 					printf("unflagged\n");
 					break;
